f0f30d382b6582c8073bc750256b65f3f572560d
prachi <prachijoshi1898@gmail.com>
2022-03-12 17:57:24 +0530

    form added

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import  ProjectDetails
+from .models import  ProjectDetails,CoalForm
+from django.shortcuts import redirect
+from .forms import Coalform
 # Create your views here.
 
 def coalIndia(request):
@@ -7,3 +9,58 @@ def coalIndia(request):
     context = {'coal':coal}
     # context = {'wells': wells, 'mylist':mylist}
     return render(request, 'home/viewVatikas.html', context )
+
+def coal(request):
+    form = Coalform()
+    if request.method == 'POST':
+        form = Coalform(request.POST, request.FILES)
+        # if form.is_valid():
+        #     form.save()
+        # name = request.POST.get('name')
+        # form = CoalForm(request.POST, request.FILES)
+        project_name = request.POST.get('project_name')
+        inputState=request.POST.get('inputState')
+        inputDistrict=request.POST.get('inputDistrict')
+        fy=request.POST.get('fy')
+        Projectstatus=request.POST.get('Projectstatus')
+        agencytype=request.POST.get('agencytype')
+        organisation_name=request.POST.get('organisation_name')
+        tender_receipt=request.POST.get('tender_receipt')
+        sector=request.POST.get('sector')
+        request_amount=request.POST.get('request_amount')
+        approved_amount=request.POST.get('approved_amount')
+        security_completion=request.POST.get('security_completion')
+        submission=request.POST.get('submission')
+        receipt=request.POST.get('receipt')
+        agenda_completion=request.POST.get('agenda_completion')
+        committee_review=request.POST.get('committee_review')
+        gm_csr_sign_off=request.POST.get('gm_csr_sign_off')
+        legal_security=request.POST.get('legal_security')
+        office_orfder_creation=request.POST.get('office_orfder_creation')
+        finance=request.POST.get('finance')
+        fund_disbursal=request.POST.get('fund_disbursal')
+        first_release_ack=request.POST.get('first_release_ack')
+        final_release_ack=request.POST.get('final_release_ack')
+        vetting_by_dp=request.POST.get('vetting_by_dp')
+        vetting_by_df=request.POST.get('vetting_by_df')
+        chairman_approval=request.POST.get('chairman_approval')
+        csr_sd_committee_review=request.POST.get('csr_sd_committee_review')
+        cil_board_review=request.POST.get('cil_board_review')
+        fcbc_completion=request.POST.get('fcbc_completion')
+        mou_finalisation=request.POST.get('mou_finalisation')
+
+        coal = CoalForm.objects.create(project_name = project_name,inputState=inputState,inputDistrict=inputDistrict,
+        fy=fy,Projectstatus=Projectstatus,agencytype=agencytype,organisation_name=organisation_name,
+        tender_receipt=tender_receipt, sector=sector,request_amount=request_amount,approved_amount=approved_amount,
+        security_completion=security_completion,submission=submission,receipt=receipt,agenda_completion=agenda_completion,
+        committee_review=committee_review,gm_csr_sign_off=gm_csr_sign_off,legal_security=legal_security,office_orfder_creation=office_orfder_creation,
+        finance=finance,fund_disbursal=fund_disbursal,first_release_ack=first_release_ack,final_release_ack=final_release_ack,
+        vetting_by_dp=vetting_by_dp,vetting_by_df=vetting_by_df,chairman_approval=chairman_approval,csr_sd_committee_review=csr_sd_committee_review,
+        cil_board_review=cil_board_review,fcbc_completion=fcbc_completion,mou_finalisation=mou_finalisation)
+        coal.save()
+        # return HttpResponseRedirect(request.path_info)
+        return redirect('/coal')
+           
+    else:
+        form = CoalForm()
+    return render(request,'home/coal.html',{})

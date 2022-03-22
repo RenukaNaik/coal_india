f807637b78be91972be58261c330afc471a3cb6c
Monika <monikapatira@gmail.com>
2022-03-22 13:24:39 +0530

    updated title of the page

--- home/models.py
+++ home/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class ProjectDetails(models.Model):
-    fy = models.CharField(max_length=255, blank=True, null=True)
-    project_status = models.CharField(max_length=255, blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    implementing_agency = models.CharField(max_length=255, blank=True, null=True)
-    tot_project_outlay = models.CharField(max_length=255, blank=True, null=True)
-    lat = models.CharField(max_length=255, blank=True, null=True)
-    lng = models.CharField(max_length=255, blank=True, null=True)
-    id = models.IntegerField(primary_key=True)
+# class ProjectDetails(models.Model):
+#     fy = models.CharField(max_length=255, blank=True, null=True)
+#     project_status = models.CharField(max_length=255, blank=True, null=True)
+#     name = models.CharField(max_length=255, blank=True, null=True)
+#     implementing_agency = models.CharField(max_length=255, blank=True, null=True)
+#     tot_project_outlay = models.CharField(max_length=255, blank=True, null=True)
+#     lat = models.CharField(max_length=255, blank=True, null=True)
+#     lng = models.CharField(max_length=255, blank=True, null=True)
+#     id = models.IntegerField(primary_key=True)
 
-    class Meta:
-        managed = False
-        db_table = 'project_details'
+#     class Meta:
+#         managed = False
+#         db_table = 'project_details'
 class CoalForm(models.Model):
     project_name=models.CharField(max_length = 250, default='', blank=True,null = True)
     inputState=models.CharField(max_length = 250, default='', blank=True,null = True)
